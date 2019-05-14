b9a1baf768040129d70a26ffaddacaa9de7add0a
pawel <pawelhulek@gmail.com>
2019-05-14 21:31:28 +0200

    Initial commit

---
+++ src/main/java/com/hulek/money/transfer/Application.java
@@ -0,0 +1,7 @@
+package com.hulek.money.transfer;
+import static spark.Spark.*;
+public class Application {
+    public static void main(String... args){
+        post("/transfers",(rq,rs)->"");
+    }
+}
